db631b04aad9bfbfaa5341c5f5bf8723ad14a340
Joel Moss <joel@developwithstyle.com>
2022-09-05 10:32:07 +0100

    fix: fixed $unproxiedState within hook

--- test/unproxied_state.test.js
+++ test/unproxied_state.test.js
@@ -1,4 +1,7 @@
-import { unproxiedStateOf, store } from 'ibiza'
+import { renderHook } from '@testing-library/react'
+
+import { useIbiza, unproxiedStateOf, store } from 'ibiza'
+import { isProxy } from '../src/store'
 
 afterEach(() => {
   store.reset()
@@ -10,7 +13,16 @@ describe('$unproxiedState', () => {
     store.state = { user: { name: 'Joel' } }
 
     expect(store.state.user.$unproxiedState).toEqual({ name: 'Joel' })
-    expect(store.state.user.$unproxiedState.isProxy).toBeUndefined()
+    expect(store.state.user.$unproxiedState[isProxy]).toBeUndefined()
+  })
+
+  test('with useIbiza', () => {
+    store.state = { user: { name: 'Joel' } }
+
+    const { result } = renderHook(() => useIbiza())
+
+    expect(result.current.user.$unproxiedState).toEqual({ name: 'Joel' })
+    expect(result.current.user.$unproxiedState[isProxy]).toBeUndefined()
   })
 })
 
@@ -19,6 +31,15 @@ describe('unproxiedStateOf()', () => {
     store.state = { user: { name: 'Joel' } }
 
     expect(unproxiedStateOf(store.state.user)).toEqual({ name: 'Joel' })
-    expect(unproxiedStateOf(store.state.user).isProxy).toBeUndefined()
+    expect(unproxiedStateOf(store.state.user)[isProxy]).toBeUndefined()
+  })
+
+  test('with useIbiza', () => {
+    store.state = { user: { name: 'Joel' } }
+
+    const { result } = renderHook(() => useIbiza())
+
+    expect(unproxiedStateOf(result.current.user)).toEqual({ name: 'Joel' })
+    expect(unproxiedStateOf(result.current.user)[isProxy]).toBeUndefined()
   })
 })
